ccdf9b64abcc90baee0029e34a98a4d2269f3139
DHS Services <ict@dhs-services.nl>
2021-11-19 16:51:03 +0100

    init module 1.0.0

---
+++ Model/Config/Source/WeightUnit.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright © Copyright 2021 DHS Services BV All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace DdJ\WeightUnitsExtend\Model\Config\Source;
+
+/**
+ * Options provider for weight units list
+ *
+ * @api
+ * @since 100.0.2
+ */
+class WeightUnit extends \Magento\Directory\Model\Config\Source\WeightUnit
+{
+    /**
+     * @inheritdoc
+     */
+    public function toOptionArray()
+    {
+        return [
+            ['value' => 'lbs', 'label' => __('lbs')],
+            ['value' => 'kgs', 'label' => __('kgs')],
+            ['value' => 'gms', 'label' => __('grams')] /* Adds grams as a unit for weights in Magento2 */
+        ];
+    }
+}
---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Copyright © Copyright 2021 DHS Services BV All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'DHSServices_WeightUnitsExtend', __DIR__);
